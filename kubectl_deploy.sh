36e7f233ba12d30e5553dc468ec9ec06c86b1a2a
mluc <my.luc@opcity.com>
2020-01-27 05:50:38 -0600

    test

--- kubectl_deploy.sh
+++ kubectl_deploy.sh
@@ -1,7 +1,7 @@
 cd /var/lib/jenkins
 sudo curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.14.6/2019-08-22/bin/linux/amd64/aws-iam-authenticator
 sudo chmod +x ./aws-iam-authenticator
-sudo mkdir -p $HOME/bin && cp ./aws-iam-authenticator $HOME/bin/aws-iam-authenticator && export PATH=$PATH:$HOME/bin
+sudo mkdir -p $HOME/bin && sudo cp ./aws-iam-authenticator $HOME/bin/aws-iam-authenticator && export PATH=$PATH:$HOME/bin
 sudo echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
 sudo aws-iam-authenticator help
 sudo kubectl config get-contexts

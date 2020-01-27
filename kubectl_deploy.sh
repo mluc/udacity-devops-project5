c628cb6afd8d8a05e9ae074020d1920509495a90
mluc <my.luc@opcity.com>
2020-01-27 03:19:59 -0600

    test

--- kubectl_deploy.sh
+++ kubectl_deploy.sh
@@ -1,4 +1,1 @@
-kubectl config set-cluster prod.us-east-1.eksctl.io --server=https://44CC0B79BD7412E0EE61DB0DF0A03258.gr7.us-east-1.eks.amazonaws.com
-kubectl config set-context project5@prod.us-east-1.eksctl.io --cluster=prod.us-east-1.eksctl.io
-kubectl config use-context project5@prod.us-east-1.eksctl.io
 kubectl get nodes

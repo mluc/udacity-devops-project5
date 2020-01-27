4a0af63cf6ddbd7a2e8951495bb45853a2bea976
mluc <my.luc@opcity.com>
2020-01-27 02:57:05 -0600

    test

--- kubectl_deploy.sh
+++ kubectl_deploy.sh
@@ -1,7 +1,8 @@
 cd /home/ubuntu
 pwd
 ls -la
-cat .kube/config
+sudo cat .kube/config
+sudo cat .aws/credentials
 kubectl config use-context project5@prod.us-east-1.eksctl.io
 kubectl config current-context
 kubectl get nodes

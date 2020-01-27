906334e24801de40edb81f336dc3278c60b12800
mluc <my.luc@opcity.com>
2020-01-27 06:02:57 -0600

    test

--- kubectl_deploy.sh
+++ kubectl_deploy.sh
@@ -1,4 +1,5 @@
 cd /var/lib/jenkins
+echo $HOME
 sudo curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.14.6/2019-08-22/bin/linux/amd64/aws-iam-authenticator
 sudo chmod +x ./aws-iam-authenticator
 sudo mkdir -p $HOME/bin && sudo cp ./aws-iam-authenticator $HOME/bin/aws-iam-authenticator && export PATH=$PATH:$HOME/bin

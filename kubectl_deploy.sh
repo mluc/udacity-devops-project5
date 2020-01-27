c004f7412e751d26ccd909a8c13d400f121792cc
mluc <my.luc@opcity.com>
2020-01-27 06:34:17 -0600

    test

--- kubectl_deploy.sh
+++ kubectl_deploy.sh
@@ -1,16 +1,13 @@
 cd /var/lib/jenkins
-export HOME=/var/lib/jenkins
-echo $HOME
 echo $PATH
-echo PATH=$PATH:/var/lib/jenkins
-echo $PATH
+export PATH=$PATH:/var/lib/jenkins
 
-#sudo curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.14.6/2019-08-22/bin/linux/amd64/aws-iam-authenticator
-#sudo chmod +x ./aws-iam-authenticator
-#sudo mkdir -p $HOME/bin && sudo cp ./aws-iam-authenticator $HOME/bin/aws-iam-authenticator && export PATH=$PATH:$HOME/bin
-#sudo echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
-#sudo echo $PATH
-#aws-iam-authenticator help
-#sudo kubectl config get-contexts
-#ls -la
-#sudo kubectl get nodes
+sudo curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.14.6/2019-08-22/bin/linux/amd64/aws-iam-authenticator
+sudo chmod +x ./aws-iam-authenticator
+sudo mkdir -p $HOME/bin && sudo cp ./aws-iam-authenticator $HOME/bin/aws-iam-authenticator && export PATH=$PATH:$HOME/bin
+sudo echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+sudo echo $PATH
+aws-iam-authenticator help
+sudo kubectl config get-contexts
+ls -la
+sudo kubectl get nodes

e712af0f1af6a4a85da39983263faaf2f28c506b
mluc <my.luc@opcity.com>
2020-01-27 02:14:24 -0600

    test

--- kubectl_deploy.sh
+++ kubectl_deploy.sh
@@ -1,3 +1,1 @@
-kubectl create deployment hello-world-project5 --image=myluc/udacity-devops:proj5
-kubectl expose deployment hello-world-project5 --type=LoadBalancer --name=my-service --port=8080
-kubectl get services my-service
+kubectl get nodes

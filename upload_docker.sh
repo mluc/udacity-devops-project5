c88a07a6a7fc70da926c5cc85f75f09a7527917b
mluc <my.luc@opcity.com>
2020-01-26 23:57:39 -0600

    add sudo

--- upload_docker.sh
+++ upload_docker.sh
@@ -2,7 +2,7 @@ dockerpath=myluc/udacity-devops:proj5
 
 
 echo "Docker ID and Image: $dockerpath"kubectl get services -o wide
-docker login --username myluc --password f6639a23-60c7-4c8b-8683-599ab8ae4332
-docker tag project5 $dockerpath
+sudo docker login --username myluc --password f6639a23-60c7-4c8b-8683-599ab8ae4332
+sudo docker tag project5 $dockerpath
 
-docker push $dockerpath
+sudo docker push $dockerpath

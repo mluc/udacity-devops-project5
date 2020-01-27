da99e182dd0f5262a2dc903ef91e9020cec09d87
mluc <my.luc@opcity.com>
2020-01-27 03:17:55 -0600

    test

--- kubectl_deploy.sh
+++ kubectl_deploy.sh
@@ -1,6 +1,4 @@
-cd /home/ubuntu
-pwd
-ls -la
-sudo cat .kube/config
-sudo cat .aws/credentials
-kubectl --server=https://44CC0B79BD7412E0EE61DB0DF0A03258.gr7.us-east-1.eks.amazonaws.com get nodes
+kubectl config set-cluster prod.us-east-1.eksctl.io --server=https://44CC0B79BD7412E0EE61DB0DF0A03258.gr7.us-east-1.eks.amazonaws.com
+kubectl config set-context project5@prod.us-east-1.eksctl.io --cluster=prod.us-east-1.eksctl.io
+kubectl config use-context project5@prod.us-east-1.eksctl.io
+kubectl get nodes

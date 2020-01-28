9d7110a36e32609864f459d3ea7429430c874584
mluc <my.luc@opcity.com>
2020-01-27 18:25:21 -0600

    test

--- kubectl_deploy.sh
+++ kubectl_deploy.sh
@@ -2,8 +2,4 @@ cd /var/lib/jenkins/
 pwd
 ls -la
 
-aws sts get-caller-identity
-sudo apt-get install awscli
-aws --version
-aws eks --region us-east-1 update-kubeconfig --name prod
-kubectl get svc
+kubectl get nodes
